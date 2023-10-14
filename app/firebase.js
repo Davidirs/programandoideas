f02db37af7ed8382d247b53b9704eb14f37ddbcc
David Reyes <reyisacc@gmail.com>
2023-10-13 20:09:33 -0400

    principio de firebase

---
+++ app/firebase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBBcEIeocoPQXUyXU-YIwCE5_N6tanDRl4",
+    authDomain: "programandoideas-bcf12.firebaseapp.com",
+    projectId: "programandoideas-bcf12",
+    storageBucket: "programandoideas-bcf12.appspot.com",
+    messagingSenderId: "471743309421",
+    appId: "1:471743309421:web:31f5a95c7f20a147df0164"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
