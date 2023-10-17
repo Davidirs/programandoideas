1a2125993c72ecc3d015ab9255bd8e2466876d91
David Reyes <reyisacc@gmail.com>
2023-10-16 20:59:41 -0400

    autenticacion con gmail facebook y github

---
+++ app/logout.js
@@ -0,0 +1,10 @@
+import { signOut } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+import { auth } from "./firebase.js";
+const logout = document.querySelector('#logout');
+if(logout){
+
+    logout.addEventListener('click', async ()=>{
+        await signOut(auth)
+        console.log("user signout");
+    })
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+import { auth } from '../../app/firebase.js'
+import { loginCheck } from '../../app/logginCheck.js'
 import '../../app/signupForm.js';
+import '../../app/signinForm.js'
+import '../../app/googleLogin.js'
+import '../../app/facebookLogin.js'
+import '../../app/githubLogin.js'
+import '../../app/logout.js'
+
+const logo = document.getElementById("logo");
+const usuario = document.getElementById("usuario");
+
+onAuthStateChanged(auth, async (user) => {
+
+    loginCheck(user);
+    if (user) {
+        if (user.photoURL !== null) {
+            logo.setAttribute("src", user.photoURL);
+            usuario.textContent = user.displayName;
+        } else {
+            usuario.textContent = user.email;
+
+        }
+
+
+        console.log(user.photoURL)
+    } else {
+        console.log("NO está logueado")
+    }
+})
